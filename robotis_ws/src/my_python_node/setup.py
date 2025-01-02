b3cde883377fbce318504b3770a9d09a493ca07e
johan-egtved <127950038+johan-egtved@users.noreply.github.com>
2025-01-02 10:27:16 +0100

    Update setup.py

--- robotis_ws/src/my_python_node/setup.py
+++ robotis_ws/src/my_python_node/setup.py
@@ -21,7 +21,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'simple_node = my_python_node.simple_node:main'
+            'simple_node = my_python_node.simple_node:main',
         ],
     },
 )
